bb9569a8be14374b3036d5baa220b69997afd0fb
Yared <penayared16@gmail.com>
2025-03-12 04:54:43 -0400

    plsgodsaveme...

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +8,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-})
+  define: {
+    'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
+  },
+});
