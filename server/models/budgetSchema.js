13c6e77d87106191b279ddd64c880b05005ae30b
Yared <penayared16@gmail.com>
2025-03-31 14:48:04 -0400

    BudgetingSchema

--- server/models/budgetSchema.js
+++ server/models/budgetSchema.js
@@ -1,7 +1,74 @@
 const mongoose = require('mongoose');
+const User = require("../models/User"); 
 
-const userSchema = new mongoose.Schema({ 
+const expenseSchema = new mongoose.Schema({ 
+    userId:
+    { type: String,
+      ref:'User',
+      required: true
+    },
 
+    category: 
+    { type: String, 
+      required: true },
 
-
+    used: 
+    { type: Number, 
+      required: true },
+    
+    budget: 
+    { type: Number,
+      required: true },
+    
 });
+
+const Expense = mongoose.model('Expense', expenseSchema);
+
+const budgetCategorySchema = new mongoose.Schema({
+    userId:
+    { type: String,
+      ref:'User',
+      required: true
+    },
+
+    name: 
+    { type: String,
+      required: true },
+
+    budget:
+    { type: Number,
+      required: true }
+});
+
+const BudgetCategory = mongoose.model('BudgetCategory', budgetCategorySchema);
+
+const budgetingSchema = new mongoose.Schema({
+    userId:
+    { type: String,
+      ref:'User',
+      required: true
+    },
+
+    categories: 
+    [{ type: mongoose.Schema.Types.ObjectId,
+       ref: BudgetCategory
+    }],
+
+    expenses: 
+    [{ type: mongoose.Schema.Types.ObjectId,
+       ref: Expense
+    }],
+
+    totalPlannedExpenses:
+    { type: Number,
+      default: 0
+    },
+
+    leftToSpend:
+    { type: Number,
+      default: 0
+    }
+});
+
+const Budgeting = mongoose.model('Budgeting', budgetingSchema);
+module.exports = Budgeting;
