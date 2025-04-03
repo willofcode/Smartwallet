493ee8bf9f579b8ca37764e8b4951310b4e56963
Yared <penayared16@gmail.com>
2025-04-02 23:01:39 -0400

    GETendpointsINPROGRESS

--- server/routes/budgetRoutes.js
+++ server/routes/budgetRoutes.js
@@ -5,6 +5,7 @@ const Budget = require("../models/budgetSchema");
 const router = express.Router();
 
 /// CRUD endpoints
+
 router.post("/post_budget", authMiddleware, async (req, res) => {
     try {
         console.log("User from middleware:", req.user);
@@ -32,12 +33,23 @@ router.post("/post_budget", authMiddleware, async (req, res) => {
     }
 });
 
-// this should either get by category or get by name...
-router.get('/get_budget/:category', async(req, res) => {
+// filtering how a user can retrieve their budget plans
+// this should either get by category or get by name... why not both
+router.get('/get_budget/:name', async(req, res) => {
     try{
-        await res.send({ message: "hi get endpoint"});
+        await res.send({ message: "hi getbyname endpoint"});
     } catch(error){
         console.error(error);
+        res.status(500).json({ message: "Cannot GET by name", error});
+    }
+});
+
+router.get('/get_budget/:category', async (req, res) => {
+    try{
+        await res.send({ message: "hi getbycategory endpoint"});
+    } catch(error){
+        console.error(error);
+        res.status(500).json({ message: "Cannot GET by category", error});
     }
 });
 
