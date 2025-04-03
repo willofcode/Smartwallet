cdb900e58a3a375eaff8d7b0d4bccdb5af25b288
thamidachoudhury1 <thamida.choudhury@gmail.com>
2025-04-03 01:13:14 -0400

    Added website with chatbot to a new branch

--- client/src/components/Chatbot.jsx
+++ client/src/components/Chatbot.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ChatbotIcon from './ChatbotIcon'
 import './Chatbot.css'
-import ChatForm from './ChatForm'
+import ChatForm from './ChatFr'
 
 const Chatbot = () => {
   return (
