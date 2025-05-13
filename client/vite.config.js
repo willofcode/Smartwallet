ce90a2ba8dbc4cbd61c5197c61ed919add35daea
William Ng <wng003@citymail.cuny.edu>
2025-05-13 19:02:13 -0400

    fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,8 +8,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  build: {
-    outDir: 'dist',
+  define: {
+    'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
   },
-  base: './',
-})
+});
